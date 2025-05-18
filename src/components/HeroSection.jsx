467ffb55483f6fb2e339caf77e3f15fade82a747
siddharth shukla <siddsanskshukla@gmail.com>
2025-05-18 18:04:36 +0530

    final

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { ArrowDown } from "lucide-react";
-import mainPic from 'src/assets/mainPic.png';
+import mainPic from '../assets/mainPic.png';
 
 export const HeroSection = () => {
   return (
